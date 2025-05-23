24bd7dd6c7c37828f2bf2001988220988c15d290
ALupo01 <amlupo01@gmail.com>
2025-05-23 05:10:26 -0300

    Cambios en Home y Footer
    Pendiente: ver cómo cargar las publicaciones de a una.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import { InstagramEmbed } from "react-social-media-embed";
+
+
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-      <p className="mt-4 text-lg">This is the main landing page of the application.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen" style={{
+      backgroundImage: `url("/entradaIMG.jpg")`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      width: "100%",
+      height: "90vh",
+      backgroundPosition: "center center"
+    }}>
+      <h1 className="text-4xl font-bold">Centro Cultural Necochea — Biblitoteca Popular "Andrés Ferreyra"</h1>
+      <p className="mt-4 text-lg">Promoviendo la cultura, educación y solidaridad en el distrito desde 1907.</p>
+      <InstagramEmbed url="https://www.instagram.com/centroculturalnecochea/" width={328}/>
     </div>
   );
 }
