4323364cbbd8097e70c66eb99f6ad5bbcdd609f2
frank <frankmolinaj9604@hotmail.com>
2025-05-27 23:50:53 -0300

    Add Link component import to Login and Register pages for improved routing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { Link } from "react-router-dom";
+
 const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { Link } from "react-router-dom";
 const Register = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
